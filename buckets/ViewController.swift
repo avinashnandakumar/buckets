26f1dd3f62360d156e80693602fcc4828022a84a
Avi <Avi@avinashs-macbook-pro.local>
2019-03-02 14:33:28 -0800

    Initial Commit

---
+++ buckets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buckets
+//
+//  Created by Avi on 3/2/19.
+//  Copyright © 2019 Heatmap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
