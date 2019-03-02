6905def09524244838aa38ddeacbf45693cffe5a
Avi <Avi@sfs-wifi-dhcp-10-142-11-142.wifi.sfsu.edu>
2019-03-02 15:31:23 -0800

    BUCKETS

--- buckets WatchKit Extension/InterfaceController.swift
+++ buckets WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,62 @@
 
 import WatchKit
 import Foundation
-
+import CoreMotion
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var xLocation: WKInterfaceLabel!
+    var motion: CMMotionManager!
+    var timer: Timer!
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        print("bad")
+        motion = CMMotionManager()
+        motion.startAccelerometerUpdates()
+        print(motion.isAccelerometerActive)
+        print(motion.isDeviceMotionAvailable)
+        startDeviceMotion()
+        print("done")
+        
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func startDeviceMotion() {
+        if motion.isDeviceMotionAvailable {
+            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
+            self.motion.showsDeviceMovementDisplay = true
+            self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+            
+            // Configure a timer to fetch the motion data.
+            self.timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
+                               block: { (timer) in
+                                if let data = self.motion.deviceMotion {
+                                    // Get the attitude relative to the magnetic north reference frame.
+                                    
+                                    let x = data.attitude.pitch
+                                    let y = data.attitude.roll
+                                    let z = data.attitude.yaw
+                                    self.xLocation.setText(String(x))
+                                    print(x)
+                                    print(y)
+                                    print(z)
+                                    // Use the motion data in your app.
+                                }
+            })
+            
+            // Add the timer to the current run loop.
+            RunLoop.current.add(self.timer!, forMode: RunLoop.Mode.default)
+        }
     }
 
 }
