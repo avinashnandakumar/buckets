e827ec015d35e1bebf90d0dad7abbcb9c4fe8eb2
Sameer99 <sameer.saptarshi>
2019-03-02 19:24:07 -0800

    accelerometer data

--- buckets WatchKit Extension/InterfaceController.swift
+++ buckets WatchKit Extension/InterfaceController.swift
@@ -13,23 +13,45 @@ import CoreMotion
 class InterfaceController: WKInterfaceController {
     
     @IBOutlet weak var xLocation: WKInterfaceLabel!
+    @IBOutlet var yLocation: WKInterfaceLabel!
+    @IBOutlet var zLocation: WKInterfaceLabel!
     var motion: CMMotionManager!
+    let queue = OperationQueue()
     var timer: Timer!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         // Configure interface objects here.
     }
     
+    override init() {
+        // Serial queue for sample handling and calculations.
+
+    }
+    
+    @IBAction func start() {
+        queue.maxConcurrentOperationCount = 1
+        queue.name = "MotionManagerQueue"
+        motion.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+            if error != nil {
+                print("Encountered error: \(error!)")
+            }
+            
+            if deviceMotion != nil {
+                self.startDeviceMotion()
+            }
+        }
+        startDeviceMotion()
+        print(motion.isAccelerometerActive)
+        print(motion.isDeviceMotionAvailable)
+    }
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        print("bad")
         motion = CMMotionManager()
         motion.startAccelerometerUpdates()
         print(motion.isAccelerometerActive)
         print(motion.isDeviceMotionAvailable)
-        startDeviceMotion()
-        print("done")
         
     }
     
@@ -45,15 +67,18 @@ class InterfaceController: WKInterfaceController {
             self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
             
             // Configure a timer to fetch the motion data.
+//            self.xLocation.setText("Out of loop")
             self.timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
                                block: { (timer) in
                                 if let data = self.motion.deviceMotion {
                                     // Get the attitude relative to the magnetic north reference frame.
-                                    
+//                                    self.xLocation.setText("In loop")
                                     let x = data.attitude.pitch
                                     let y = data.attitude.roll
                                     let z = data.attitude.yaw
                                     self.xLocation.setText(String(x))
+                                    self.yLocation.setText(String(y))
+                                    self.zLocation.setText(String(z))
                                     print(x)
                                     print(y)
                                     print(z)
@@ -62,7 +87,11 @@ class InterfaceController: WKInterfaceController {
             })
             
             // Add the timer to the current run loop.
+//            self.xLocation.setText("Out of loop 2")
             RunLoop.current.add(self.timer!, forMode: RunLoop.Mode.default)
+        } else {
+            self.xLocation.setText("Not available")
+
         }
     }
 
