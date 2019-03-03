78bb79eb5e789db1185d9715485c58f8de0cc90d
Sameer99 <sameer.saptarshi>
2019-03-03 05:53:02 -0800

    1/20 sampling interval

--- buckets WatchKit Extension/InterfaceController.swift
+++ buckets WatchKit Extension/InterfaceController.swift
@@ -9,15 +9,29 @@
 import WatchKit
 import Foundation
 import CoreMotion
+import HealthKit
+import WatchConnectivity
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
     @IBOutlet weak var xLocation: WKInterfaceLabel!
     @IBOutlet var yLocation: WKInterfaceLabel!
     @IBOutlet var zLocation: WKInterfaceLabel!
     var motion: CMMotionManager!
     let queue = OperationQueue()
-    var timer: Timer!
+    var timer: Timer?
+    var count = 0
+//    var allData = [[[Any]]]()
+//    var shot = [[Any]]()
+    var allData = [String]()
+    var workout: HKWorkoutSession!
+    var active = false
+    var session : WCSession!
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -30,9 +44,23 @@ class InterfaceController: WKInterfaceController {
     }
     
     @IBAction func start() {
-        queue.maxConcurrentOperationCount = 1
-        queue.name = "MotionManagerQueue"
-        motion.startDeviceMotionUpdates(to: queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
+//        print("helllllllllo")
+        //print(String(self.active))
+        //print(String(self.timer == nil))
+        //if (!(self.active) || (!(self.timer == nil))) {
+        self.motion = CMMotionManager()
+        self.motion.startAccelerometerUpdates()
+//        print(self.motion.isAccelerometerActive)
+//        print(self.motion.isDeviceMotionAvailable)
+        
+        let workoutConfiguration = HKWorkoutConfiguration()
+        workoutConfiguration.activityType = .other
+        let healthStore = HKHealthStore()
+        self.workout = try? HKWorkoutSession(healthStore: healthStore, configuration: workoutConfiguration)
+        self.workout.startActivity(with: Date())
+        self.queue.maxConcurrentOperationCount = 1
+        self.queue.name = "MotionManagerQueue"
+        self.motion.startDeviceMotionUpdates(to: self.queue) { (deviceMotion: CMDeviceMotion?, error: Error?) in
             if error != nil {
                 print("Encountered error: \(error!)")
             }
@@ -41,17 +69,95 @@ class InterfaceController: WKInterfaceController {
                 self.startDeviceMotion()
             }
         }
-        startDeviceMotion()
-        print(motion.isAccelerometerActive)
-        print(motion.isDeviceMotionAvailable)
+//        shot = [[String]]()
+        count = 0
+        self.timer = nil
+        self.startDeviceMotion()
+        //allData.append(shot)
+//        print(self.motion.isAccelerometerActive)
+//        print(self.motion.isDeviceMotionAvailable)
+        //}
     }
+    
+    @IBAction func stop() {
+        
+        for element in allData {
+            print(element, terminator:", ")
+        }
+        self.allData = [String]()
+//        print("ROASTER")
+        //let fileUrl = NSURL(fileURLWithPath: "/foo123.plist") // Your path here
+//        let listOfTasks = [["Hi", "Hello", "12:00"], ["Hey there", "What's up?", "3:17"]]
+        
+        // Save to file
+        //(samples as NSArray).write(to: fileUrl as URL, atomically: true)
+        
+        // Read from file
+        //let savedArray = NSArray(contentsOf: fileUrl as URL) as! [[[String]]]
+        
+        //print(savedArray[0][0][0])
+//        if (WCSession.default.isReachable) {
+//            // this is a meaningless message, but it's enough for our purposes
+//            let message = ["Message": allData] //[0][0][0]]
+//            WCSession.default.sendMessage(message, replyHandler: nil)
+//            allData = [String]()
+//        }
+//        let documentsDirectoryPathString = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+//        let documentsDirectoryPath = NSURL(string: documentsDirectoryPathString)!
+//
+//        let jsonFilePath = documentsDirectoryPath.appendingPathComponent("sameer/test_adi_sameer_avi.json")
+//        let fileManager = FileManager.default
+//        var isDirectory: ObjCBool = false
+//
+//        // creating a .json file in the Documents folder
+//        if !fileManager.fileExists(atPath: (jsonFilePath?.absoluteString)!, isDirectory: &isDirectory) {
+//            let created = fileManager.createFile(atPath: (jsonFilePath?.absoluteString)!, contents: nil, attributes: nil)
+//            if created {
+//                print("File created ")
+//            } else {
+//                print("Couldn't create file for some reason")
+//            }
+//        } else {
+//            print("File already exists")
+//        }
+//
+//
+//        // creating JSON out of the above array
+//        var jsonData: NSData!
+//        do {
+//            jsonData = try JSONSerialization.data(withJSONObject: samples, options: JSONSerialization.WritingOptions()) as NSData
+//            let jsonString = String(data: jsonData as Data, encoding: String.Encoding.utf8)
+//            print(jsonString ?? "json string didn't work")
+//        } catch let error as NSError {
+//            print("Array to JSON conversion failed: \(error.localizedDescription)")
+//        }
+//
+//        // Write that JSON to the file created earlier
+////        let jsonFilePath = documentsDirectoryPath.appendingPathComponent("test.json")
+//        do {
+//            let file = try FileHandle(forWritingTo: jsonFilePath!)
+//            file.write(jsonData as Data)
+//            print("JSON data was written to teh file successfully!")
+//        } catch let error as NSError {
+//            print("Couldn't write to file: \(error.localizedDescription)")
+//        }
+    }
+    
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        // This method is called when watch view controller is about to be visible to usersuper.willActivate()
         super.willActivate()
-        motion = CMMotionManager()
-        motion.startAccelerometerUpdates()
-        print(motion.isAccelerometerActive)
-        print(motion.isDeviceMotionAvailable)
+        if (timer == nil) {
+            motion = CMMotionManager()
+            motion.startAccelerometerUpdates()
+//            print(motion.isAccelerometerActive)
+//            print(motion.isDeviceMotionAvailable)
+            
+        }
+        if (WCSession.isSupported()) {
+            session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
         
     }
     
@@ -61,37 +167,114 @@ class InterfaceController: WKInterfaceController {
     }
     
     func startDeviceMotion() {
-        if motion.isDeviceMotionAvailable {
-            self.motion.deviceMotionUpdateInterval = 1.0 / 60.0
+        //stop()
+        if motion.isDeviceMotionAvailable{// && self.timer == nil {
+            self.motion.deviceMotionUpdateInterval = 1.0 / 10.0
+            self.motion.accelerometerUpdateInterval = 1.0 / 10.0
+
             self.motion.showsDeviceMovementDisplay = true
             self.motion.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
             
             // Configure a timer to fetch the motion data.
 //            self.xLocation.setText("Out of loop")
-            self.timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
-                               block: { (timer) in
-                                if let data = self.motion.deviceMotion {
-                                    // Get the attitude relative to the magnetic north reference frame.
-//                                    self.xLocation.setText("In loop")
-                                    let x = data.attitude.pitch
-                                    let y = data.attitude.roll
-                                    let z = data.attitude.yaw
-                                    self.xLocation.setText(String(x))
-                                    self.yLocation.setText(String(y))
-                                    self.zLocation.setText(String(z))
-                                    print(x)
-                                    print(y)
-                                    print(z)
-                                    // Use the motion data in your app.
-                                }
-            })
+            self.active = true
+//            timer.invalidate()
+            //self.timer = Timer.scheduledTimerWithTimeInterval((1.0 / 60.0),selector: "onTick:", userInfo: nil,  repeats: true)
+            if (self.timer == nil) {
+                self.timer = Timer.scheduledTimer(timeInterval: (1.0 / 20.0),
+                                                  target: self,
+                                                  selector: #selector(InterfaceController.onTick),
+                                                  userInfo: nil,
+                                                  repeats: true)
+//                self.timer?.tolerance = 0.2
+            }
+            
+            
+            
+            
+//            self.timer = Timer.(fire: Date(), interval: (1.0 / 60.0), repeats: true,
+//                               block: { _ in @escaping (timer); in
+//                                if let data = self.motion.deviceMotion {
+//                                    // Get the attitude relative to the magnetic north reference frame.
+////                                    self.xLocation.setText("In loop")
+//                                    if (self.active) {
+//                                        let x = data.attitude.pitch
+//                                        let y = data.attitude.roll
+//                                        let z = data.attitude.yaw
+//                                        self.xLocation.setText(String(x))
+//                                        self.yLocation.setText(String(y))
+//                                        self.zLocation.setText(String(z))
+//                                        print(x)
+//                                        print(y)
+//                                        print(z)
+//                                    } else {
+////                                        self.timer.invalidate()
+////                                        self.timer = nil
+////                                        self.active = false
+//                                    }
+//                                    // Use the motion data in your app.
+//                                }
+//
+//            })
             
             // Add the timer to the current run loop.
 //            self.xLocation.setText("Out of loop 2")
-            RunLoop.current.add(self.timer!, forMode: RunLoop.Mode.default)
+            //RunLoop.current.add(self.timer!, forMode: RunLoop.Mode.default)
         } else {
-            self.xLocation.setText("Not available")
+//            self.xLocation.setText("Not available")
 
+        }
+    }
+    
+    @objc func onTick(){
+        if let data = self.motion.deviceMotion {
+            // Get the attitude relative to the magnetic north reference frame.
+            //                                    self.xLocation.setText("In loop")
+            //if (self.active) {
+            count+=1
+            let x = data.attitude.pitch
+            let y = data.attitude.roll
+            let z = data.attitude.yaw
+            let sample = [data.attitude.pitch, data.attitude.roll, data.attitude.yaw,
+                       data.userAcceleration.x, data.userAcceleration.y, data.userAcceleration.z,
+                       data.rotationRate.x, data.rotationRate.y, data.rotationRate.z,
+                       data.gravity.x, data.gravity.y, data.gravity.z]
+//            let sample = [data.userAcceleration.x, data.userAcceleration.y, data.userAcceleration.z]
+            var stringified = [String]()
+            for s in sample {
+                stringified.append(String(s))
+            }
+            if (WCSession.default.isReachable) {
+                // this is a meaningless message, but it's enough for our purposes
+                let message = ["x": stringified[0], "y": stringified[1], "z": stringified[2],
+                               "accelX": stringified[3], "accelY": stringified[4], "accelZ": stringified[5],
+                               "rotX": stringified[6], "rotY": stringified[7], "rotZ": stringified[8],
+                               "gravX": stringified[9], "gravY": stringified[10], "gravZ": stringified[11]] //[0][0][0]]
+                WCSession.default.sendMessage(message, replyHandler: nil)
+            }
+//            for s in sample {
+//
+//                allData.append(String(s))
+//            }
+            //print(stringified)
+//            shot.append(stringified)
+            //print(shot)
+//            print(sample)
+            self.xLocation.setText(String(x))
+            self.yLocation.setText(String(y))
+            self.zLocation.setText(String(z))
+//            print(x)
+//            print(y)
+//            print(z)
+//            print(count)
+            //}
+            if count >= 80 {
+//                allData.append(shot)
+                self.timer?.invalidate()
+                self.timer = nil
+            }
+            
+            
         }
     }
 
--- buckets/ViewController.swift
+++ buckets/ViewController.swift
@@ -7,14 +7,122 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import WatchConnectivity
+class ViewController: UIViewController, WCSessionDelegate {
+    var session : WCSession!
+    @IBOutlet var xView: UILabel!
+    @IBOutlet var yView: UILabel!
+    @IBOutlet var zView: UILabel!
+    var allData = String()
+    var count = 0
+    var total = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        if WCSession.isSupported() {
+            session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        //let counterValue = message["counterValue"] as? String
+        
+        //Use this to update the UI instantaneously (otherwise, takes a little while)
+        //        dispatch_async(Dispatch().dispatch_get_main_queue()) {
+        //            self.roast.text = "UR BAD"
+        //            //self.counterData.append(counterValue!)
+        //            //self.mainTableView.reloadData()
+        //        }
+        let x = message["x"] as? String
+        let y = message["y"] as? String
+        let z = message["z"] as? String
+        let accelX = message["accelX"] as? String
+        let accelY = message["accelY"] as? String
+        let accelZ = message["accelZ"] as? String
+        let rotX = message["rotX"] as? String
+        let rotY = message["rotY"] as? String
+        let rotZ = message["rotZ"] as? String
+        let gravX = message["gravX"] as? String
+        let gravY = message["gravY"] as? String
+        let gravZ = message["gravZ"] as? String
+        DispatchQueue.main.async {
+            // do something
+            for s in [x,y,z,accelX, accelY, accelZ, rotX, rotY, rotZ, gravX, gravY, gravZ] {
+                self.allData = self.allData + s! + ", "
+                self.total += 1
+            }
+            
+//            self.allData.append(x!)
+//            self.allData.append(y!)
+//            self.allData.append(z!)
+//            self.allData.append(accelX!)
+//            self.allData.append(accelY!)
+//            self.allData.append(accelZ!)
+//            self.allData.append(rotX!)
+//            self.allData.append(rotY!)
+//            self.allData.append(rotZ!)
+//            self.allData.append(gravX!)
+//            self.allData.append(gravY!)
+//            self.allData.append(gravZ!)
 
+
+            //self.allData += messageValue!
+            //self.xView.text = "ROAST"
+            //print(messageValue[0])
+//            self.xView.text = accelX
+//            self.yView.text = accelY
+//            self.zView.text = accelZ
+        }
+    }
+    
+    @IBAction func saveFile(_ sender: Any) {
+//        let documentsDirectoryPathString = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+//        let documentsDirectoryPath = NSURL(string: documentsDirectoryPathString)!
+//        let filePath = documentsDirectoryPath.appendingPathComponent("test_roast" + String(count) + ".json")
+//        count += 1
+//        (allData as NSArray).write(to: filePath as! URL, atomically: true)
+//
+//        let savedArray = NSArray(contentsOf: filePath as! URL) as! [String]
+//        for val in allData {
+//            print(val, terminator: ", ")
+//        }
+//        let str = "Super long string here"
+        let filename = getDocumentsDirectory().appendingPathComponent("roaster" + String(self.count) + ".txt")
+        self.count += 1
+        
+        do {
+            try allData.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
+            allData = String()
+            print("coore")
+        } catch {
+            print("we are bad")
+            // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+        }
+        self.xView.text = "DONE!"
+        self.yView.text = String(self.total)
+
+
+
+
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 
 }
 
